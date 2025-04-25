6b0ef272dc5063d67674d6c16e2fcc44ae15c011
tanachock <tanachock.saelo@gmail.com>
2025-04-25 17:13:21 +0700

    update5

--- src/app/page2/page2.component.ts
+++ src/app/page2/page2.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import liff from '@line/liff';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-page2',
   templateUrl: './page2.component.html',
@@ -10,11 +10,17 @@ export class Page2Component {
 
   event = "";
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
     this.route.queryParams.subscribe(params => {
       this.event = params['event'];
+    });
+
+    liff.getFriendship().then((status) => {
+      if (status.friendFlag == true) {
+        this.router.navigate(['/page1'], { queryParams: { event: this.event } });
+      }
     });
   }
   // addFriend() {
@@ -33,11 +39,11 @@ export class Page2Component {
   // }
 
   addFriend() {
-    liff.logout();
     liff.openWindow({
-      url: 'https://liff.line.me/2007317200-7nLJVveG?event=' + this.event,
-      external: false
+      url: 'https://line.me/R/ti/p/@893iaere',
+      external: true
     });
 
+    alert('หลังจากเพิ่มเพื่อนเสร็จแล้ว กรุณากลับมายังแอป LIFF เพื่อดำเนินการต่อ');
   }
 }
